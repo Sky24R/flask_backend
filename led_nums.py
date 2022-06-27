cc34d69de011571da3626a361e545e61ec9a5915
Sky24R <838495693@qq.com>
2022-06-27 15:48:05 +0800

    v2

--- led_nums.py
+++ led_nums.py
@@ -165,8 +165,8 @@ def cut(img ):
         points.append(point)
 
 
-    # cv2.imshow('img',img)
-    # cv2.waitKey()
+    cv2.imshow('img',img)
+    cv2.waitKey()
     cv2.destroyAllWindows()
 
     return points
